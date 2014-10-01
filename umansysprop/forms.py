5f56135a9c5554c894dec6ef579509b8151b96a6
Dave Hughes <dave@waveform.org.uk>
2014-10-01 18:12:20 +0100

    Added SMILESDictField
    Also created SubForm class for use in internal form classes

--- umansysprop/forms.py
+++ umansysprop/forms.py
@@ -35,7 +35,7 @@ import decimal
 
 import pybel
 from flask import request
-from flask.ext.wtf import Form
+from flask.ext.wtf import Form as BaseForm
 from wtforms.fields import (
     Field,
     BooleanField,
@@ -83,6 +83,14 @@ from wtforms.widgets import TextInput
 from wtforms.widgets.html5 import NumberInput
 
 from .html import html, literal, content, tag
+
+
+Form = BaseForm
+
+
+class SubForm(BaseForm):
+    def __init__(self, csrf_enabled=False, *args, **kwargs):
+        super(SubForm, self).__init__(*args, csrf_enabled=False, **kwargs)
 
 
 def smiles(s):
@@ -162,16 +170,28 @@ class SMILESField(Field):
             self.data = None
 
 
-class SMILESListField(FormField):
+class SMILESDictField(FormField):
     """
-    Represents a compound input which accepts a list of SMILES strings
-    representing chemical compounds, or a file containing one SMILES string per
-    line. The field's data is returned as a sequence of OpenBabel Molecule
-    objects
+    Represents a compound input which defines a mapping of SMILES strings to
+    floating point values, or accepts a file upload containing one SMILES
+    string and one floating point value separated by whitespace, per line. The
+    field's data is returned as a mapping of OpenBabel Molecule objects to
+    float values.
+
+    Additional keyword arguments introduced by this class are:
+
+    :param entry_label:
+        Provides the label appearing above the SMILES text entry field
+
+    :param data_label:
+        Provides the label appearing above the floating point entry field
+
+    :param upload_label:
+        Provides the label appearing beside the file upload field
 
     :param compounds:
         If provided, a sequence of ``(value, label)`` tuples which can be
-        selected by drop-down from the text field. Defaults to an empty
+        selected by drop-down from the text-entry field. Defaults to an empty
         sequence.
 
     :param min_entries:
@@ -181,33 +201,46 @@ class SMILESListField(FormField):
         The maximum number of entries permitted in the field. Defaults to 100.
     """
 
-    def __init__(self, label=None, validators=None, compounds=None,
-            separator='-', min_entries=0, max_entries=100, **kwargs):
+    def __init__(self, label=None, entry_label=None, data_label=None,
+            upload_label=None, validators=None, compounds=None, separator='-',
+            min_entries=0, max_entries=100, **kwargs):
 
-        class ListForm(Form):
-            smiles = FieldList(
-                SMILESField(label, validators=validators, compounds=compounds),
+        class MapForm(SubForm):
+            smiles = SMILESField(entry_label, compounds=compounds)
+            data = FloatField(data_label)
+
+        class ListForm(SubForm):
+            entry = FieldList(
+                FormField(MapForm),
                 min_entries=min_entries, max_entries=max_entries)
-            upload =FileField(label)
+            upload = FileField(upload_label)
 
-            def __init__(self, csrf_enabled=False, *args, **kwargs):
-                super(ListForm, self).__init__(*args, csrf_enabled=False, **kwargs)
-
-        super(SMILESListField, self).__init__(
+        super(SMILESDictField, self).__init__(
                 ListForm, label, separator=separator, **kwargs)
 
     @property
     def data(self):
         if self.form.upload.name in request.files:
             try:
-                result = [smiles(s.strip()) for i, s in enumerate(
-                    request.files[self.form.upload.name].read().splitlines())]
+                result = {
+                    smiles(key): float(value)
+                    for i, _line in enumerate(
+                        request.files[self.form.upload.name].read().splitlines(),
+                        start=1)
+                    for line in (_line.strip(),)
+                    for key, value in (line.split(None, 1),)
+                    if line and not line.startswith('#')
+                    }
             except ValueError as e:
-                raise ValueError('%s on line %d' % (str(e), i))
+                e.args += ('on line %d' % i)
+                raise
         else:
-            result = self.form.smiles.data
-        min_len = self.form.smiles.min_entries
-        max_len = self.form.smiles.max_entries
+            result = {
+                e.smiles.data: e.data.data
+                for e in self.form.entry
+                }
+        min_len = self.form.entry.min_entries
+        max_len = self.form.entry.max_entries
         if len(result) < min_len:
             raise ValueError(
                 'not enough entries (%d); expected at least %d' %
@@ -219,8 +252,213 @@ class SMILESListField(FormField):
         return result
 
     def __call__(self, **kwargs):
-        if not len(self.form.smiles):
-            self.form.smiles.append_entry()
+        if not len(self.form.entry):
+            self.form.entry.append_entry()
+        # XXX Layout specific to UManSysProp
+        return tag.div(
+            tag.div(
+                tag.div(
+                    tag.label(
+                        tag.input(
+                            type='checkbox',
+                            value='file'
+                            ),
+                        ' upload file',
+                        ),
+                    class_='small-12 columns'
+                    ),
+                class_='row'
+                ),
+            tag.div(
+                tag.div(
+                    self.form.upload,
+                    class_='small-12 columns'
+                    ),
+                class_='row',
+                data_toggle='fieldset-upload'
+                ),
+            tag.div(
+                tag.div(
+                    self.form.entry[0].smiles.label(class_='inline'),
+                    class_='small-6 columns'
+                    ),
+                tag.div(
+                    self.form.entry[0].data.label(class_='inline'),
+                    class_='medium-4 small-3 columns'
+                    ),
+                tag.div(
+                    tag.a('Add', class_='button tiny right', data_toggle='fieldset-add-row'),
+                    class_='medium-2 small-3 columns clearfix'
+                    ),
+                class_='row',
+                data_toggle='fieldset-entry'
+                ),
+            (tag.div(
+                tag.div(
+                    entry.smiles,
+                    class_='small-6 columns'
+                    ),
+                tag.div(
+                    entry.data,
+                    class_='medium-4 small-3 columns'
+                    ),
+                tag.div(
+                    tag.a('Remove', class_='button tiny right', data_toggle='fieldset-remove-row'),
+                    class_='medium-2 small-3 columns clearfix'
+                    ),
+                class_='row',
+                data_toggle='fieldset-entry'
+                ) for entry in self.form.entry),
+            id=self.id,
+            data_toggle='fieldset',
+            data_freeid=len(self.form.entry)
+            )
+
+    @property
+    def scripts(self):
+        template = """\
+$('div#%(id)s').each(function() {
+    var $field = $(this);
+    var $check = $field.find(':checkbox');
+    var $add = $field.find('a[data-toggle=fieldset-add-row]');
+    var $remove = $field.find('a[data-toggle=fieldset-remove-row]');
+    var $upload = $field.find('div[data-toggle=fieldset-upload]');
+    var freeid = parseInt($field.data('freeid'));
+
+    $add.click(function() {
+        // Find the last row and clone it
+        var $oldrow = $field.find('div[data-toggle=fieldset-entry]:last');
+        var $row = $oldrow.clone(true);
+        // Re-write the ids of the input in the row
+        $row.find(':input').each(function() {
+            var newid = $(this).attr('id').replace(
+                /%(id)s-entry-(\d{1,4})/,
+                '%(id)s-entry-' + freeid);
+            $(this)
+                .attr('name', newid)
+                .attr('id', newid)
+                .val('')
+                .removeAttr('checked');
+        });
+        $oldrow.after($row);
+        freeid++;
+    });
+
+    $remove.click(function() {
+        if ($field.find('div[data-toggle=fieldset-entry]').length > 2) {
+            var thisRow = $(this).closest('div[data-toggle=fieldset-entry]');
+            thisRow.remove();
+        }
+    });
+
+    $check.change(function() {
+        // Refresh the entry matches
+        var $entry = $field.find('div[data-toggle=fieldset-entry]');
+
+        var $show = this.checked ? $upload : $entry;
+        var $hide = this.checked ? $entry : $upload;
+        $hide.fadeOut('fast', function() {
+            $hide
+                .find(':input')
+                    .prop('disabled', true);
+            $show
+                .find(':input')
+                    .prop('disabled', false)
+                .end()
+                    .fadeIn('fast');
+        });
+    });
+
+    if ($field.find(':file').val()) {
+        $check.prop('checked', true);
+        $field.find('div[data-toggle=fieldset-entry]')
+            .hide().find(':input').prop('disabled', true);
+    }
+    else {
+        $check.prop('checked', false);
+        $upload
+            .hide().find(':input').prop('disabled', true);
+    }
+});
+"""
+        return literal('\n'.join(
+            [tag.script(literal(template % {'id': self.id}))] +
+            [entry.smiles.scripts for entry in self.form.entry]
+            ))
+
+
+class SMILESListField(FormField):
+    """
+    Represents a compound input which defines a list of SMILES strings
+    representing chemical compounds, or accepts a file upload containing one
+    SMILES string per line. The field's data is returned as a sequence of
+    OpenBabel Molecule objects.
+
+    Additional keyword arguments introduced by this class are:
+
+    :param entry_label:
+        Provides the label appearing above the SMILES text entry field
+
+    :param upload_label:
+        Provides the label appearing beside the file upload field
+
+    :param compounds:
+        If provided, a sequence of ``(value, label)`` tuples which can be
+        selected by drop-down from the text-entry field. Defaults to an empty
+        sequence.
+
+    :param min_entries:
+        The minimum number of entries permitted in the field. Defaults to 0.
+
+    :param max_entries:
+        The maximum number of entries permitted in the field. Defaults to 100.
+    """
+
+    def __init__(self, label=None, entry_label=None, upload_label=None,
+            validators=None, compounds=None, separator='-', min_entries=0,
+            max_entries=100, **kwargs):
+
+        class ListForm(SubForm):
+            entry = FieldList(
+                SMILESField(entry_label, validators=validators, compounds=compounds),
+                min_entries=min_entries, max_entries=max_entries)
+            upload = FileField(upload_label)
+
+        super(SMILESListField, self).__init__(
+                ListForm, label, separator=separator, **kwargs)
+
+    @property
+    def data(self):
+        if self.form.upload.name in request.files:
+            try:
+                result = [
+                    smiles(line)
+                    for i, _line in enumerate(
+                        request.files[self.form.upload.name].read().splitlines(),
+                        start=1
+                        )
+                    for line in (_line.strip(),)
+                    if line and not line.startswith('#')
+                    ]
+            except ValueError as e:
+                raise ValueError('%s on line %d' % (str(e), i))
+        else:
+            result = self.form.entry.data
+        min_len = self.form.entry.min_entries
+        max_len = self.form.entry.max_entries
+        if len(result) < min_len:
+            raise ValueError(
+                'not enough entries (%d); expected at least %d' %
+                (len(result), min_len))
+        if len(result) > max_len:
+            raise ValueError(
+                'too many entries (%d); maximum %d' %
+                (len(result), max_len))
+        return result
+
+    def __call__(self, **kwargs):
+        if not len(self.form.entry):
+            self.form.entry.append_entry()
         # XXX Layout specific to UManSysProp
         return tag.div(
             tag.div(
@@ -259,10 +497,10 @@ class SMILESListField(FormField):
                     ),
                 class_='row',
                 data_toggle='fieldset-entry'
-                ) for field in self.form.smiles),
+                ) for field in self.form.entry),
             id=self.id,
             data_toggle='fieldset',
-            data_freeid=len(self.form.smiles)
+            data_freeid=len(self.form.entry)
             )
 
     @property
@@ -283,8 +521,8 @@ $('div#%(id)s').each(function() {
         // Re-write the ids of the input in the row
         $row.find(':input').each(function() {
             var newid = $(this).attr('id').replace(
-                /%(id)s-smiles-(\d{1,4})/,
-                '%(id)s-smiles-' + freeid);
+                /%(id)s-entry-(\d{1,4})/,
+                '%(id)s-entry-' + freeid);
             $(this)
                 .attr('name', newid)
                 .attr('id', newid)
@@ -334,7 +572,7 @@ $('div#%(id)s').each(function() {
 """
         return literal('\n'.join(
             [tag.script(literal(template % {'id': self.id}))] +
-            [field.scripts for field in self.form.smiles]
+            [field.scripts for field in self.form.entry]
             ))
 
 
@@ -345,12 +583,12 @@ class FloatRangeField(FormField):
     inclusive end-points. In either case, the field returns a sequence of
     floating point values as its data.
 
-    :param max_count:
+    :param max_entries:
         Defines the maximum number of values in the sequence. Defaults to 1000.
     """
 
     def __init__(
-            self, label=None, validators=None, separator='-', max_count=1000,
+            self, label=None, validators=None, separator='-', max_entries=1000,
             **kwargs):
         validators = validators or []
         self._min = None
@@ -361,12 +599,12 @@ class FloatRangeField(FormField):
                 self._max = v.max
                 break
 
-        class RangeForm(Form):
+        class RangeForm(SubForm):
             count = IntegerField(
                 'values', default=1,
                 validators=
                     [v for v in validators if isinstance(v, InputRequired)] +
-                    [NumberRange(min=1, max=max_count)]
+                    [NumberRange(min=1, max=max_entries)]
                 )
             start = FloatField(
                 'values from', default=kwargs.get('default'),
@@ -376,9 +614,6 @@ class FloatRangeField(FormField):
                 validators=
                     [v for v in validators if not isinstance(v, InputRequired)]
                 )
-
-            def __init__(self, csrf_enabled=False, *args, **kwargs):
-                super(RangeForm, self).__init__(*args, csrf_enabled=False, **kwargs)
 
             def validate(self):
                 if not super(RangeForm, self).validate():
