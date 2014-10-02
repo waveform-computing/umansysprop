73ef6fa8b338c867fb820a5595472b7b09a2e0e6
Dave Hughes <dave@waveform.org.uk>
2014-10-02 17:36:25 +0100

    Add type conversion into the client
    Client now strips out the 'tables' definitions and performs type
    conversions on row and column keys

--- umansysprop/client.py
+++ umansysprop/client.py
@@ -79,5 +79,30 @@ def {name}(self, {params}):
                     'Content-Type': 'application/json',
                     })
         response.raise_for_status()
-        return response.json()['result']
+        result = response.json()
+        # Perform data-type conversions
+        tables = result.pop('tables')
+        conversions = {
+            'int':   int,
+            'float': float,
+            'str':   lambda x: x,
+            }
+        row_conversions = {
+            table: conversions[tables[table]['rows'][1]]
+            for table in tables
+            }
+        col_conversions = {
+            table: conversions[tables[table]['cols'][1]]
+            for table in tables
+            }
+        return {
+            table: {
+                col_conversions[table](col_key): {
+                    row_conversions[table](row_key): value
+                    for row_key, value in rows.items()
+                    }
+                for col_key, rows in cols.items()
+                }
+            for table, cols in result.items()
+            }
 
