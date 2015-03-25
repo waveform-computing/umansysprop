378c57f91e31ded6edd84bbc6424400880f09422
Dave Hughes <dave@waveform.org.uk>
2015-03-25 19:06:18 +0000

    Improve result and table representation
    Table representation can now include multi-dimensional column and row
    keys. Renderers are improved to utilize column and row spanning.

--- umansysprop/renderers.py
+++ umansysprop/renderers.py
@@ -24,11 +24,16 @@ from __future__ import (
     division,
     )
 str = type('')
+try:
+    from itertools import izip as zip
+except ImportError:
+    pass
 
 
 import sys
 import io
 import csv
+import pickle
 import tempfile
 
 import xlsxwriter as xl
@@ -66,122 +71,189 @@ def render(mimetype, obj, **kwargs):
 
 
 @register('application/json', 'JSON file')
-def render_json(obj, **kwargs):
-    return json.dumps(obj, **kwargs)
+def render_json(results, **kwargs):
+
+    def render_table(table):
+        return {
+            'name': table.name,
+            'title': table.title,
+            'data': [
+                {
+                    'key': (row_key, col_key),
+                    'value': table.data[(row_key, col_key)],
+                    }
+                for row_key in table.rows
+                for col_key in table.cols
+                ]
+            }
+
+    return json.dumps([render_table(table) for table in results], **kwargs)
 
 
-@register('application/csv', 'CSV file', {
+@register('application/octet-stream', 'Python pickle')
+def render_pickle(results, **kwargs):
+    for table in results:
+        # Force evaluation of the data and wipe the function reference (it's
+        # no longer needed and typically prevents serialization by being a
+        # lambda of some sorts)
+        table.data
+        table._func = None
+    return pickle.dumps(results, protocol=0)
+
+
+@register('application/xml', 'XML file', headers={
+        'Content-Disposition': 'attachment; filename=umansysprop.xml',
+        })
+def render_xml(results, **kwargs):
+    tag = TagFactory(xml=True)
+
+    def render_table(table):
+        return tag.table(
+            tag.columns(
+                (tag.column(id=str(col_key)) for col_key in table.cols),
+                title=table.cols_title
+                ),
+            tag.rows((
+                tag.row(
+                    (tag.data(value=table.data[(row_key, col_key)]) for col_key in table.cols),
+                    id=str(row_key)
+                ) for row_key in table.rows),
+                title=table.rows_title
+                ),
+            title=table.title,
+            name=table.name,
+            )
+
+    return tag.tables(render_table(table) for table in results)
+
+
+@register('application/csv', 'CSV file', headers={
         'Content-Disposition': 'attachment; filename=umansysprop.zip',
         })
-def render_csv(obj, **kwargs):
+def render_csv(results, **kwargs):
 
-    def process_table(table_id):
-        col_keys = sorted(obj[table_id].keys())
-        row_keys = sorted(obj[table_id][col_keys[0]].keys())
+    def render_table(table):
         # Deal with incompatibility between Py2 and Py3's CSV writer
         if sys.version_info.major == 3:
             stream = io.StringIO(newline='')
         else:
             stream = io.BytesIO()
         writer = csv.writer(stream)
-        writer.writerow([obj['tables'][table_id]['rows'][2]] + col_keys)
-        for row_key in row_keys:
-            writer.writerow([row_key] + [obj[table_id][col_key][row_key] for col_key in col_keys])
+        for dim in range(table.col_dims):
+            writer.writerow(
+                [''] * table.row_dims +
+                [col_key[dim] for col_key in table.cols_iter]
+                )
+        for data_row, row_key in zip(table.rows, table.rows_iter):
+            writer.writerow(
+                list(row_key) +
+                [table.data[(data_row, data_col)] for data_col in table.cols]
+                )
         stream.seek(0)
         return stream
 
     with io.BytesIO() as stream:
         with ZipFile(stream, 'w', compression=ZIP_DEFLATED) as archive:
-            archive.comment = '\n'.join(
-                obj['tables'][table_id]['title']
-                for table_id in obj['tables']
-                ).encode('utf-8')
-            for table_id in obj['tables']:
-                archive.write(process_table(table_id), obj['tables'][table_id]['title'] + '.csv')
+            archive.comment = '\n'.join(table.title for table in results).encode('utf-8')
+            for table in results:
+                archive.write(render_table(table), '%s.csv' % table.name)
         return stream.getvalue()
 
 
-@register('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'Excel file', {
+@register('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'Excel file', headers={
         'Content-Disposition': 'attachment; filename=umansysprop.xlsx',
         })
-def render_xlsx(obj, **kwargs):
+def render_xlsx(results, **kwargs):
     stream = io.BytesIO()
     workbook = xl.Workbook(stream, {'in_memory': True})
     bold = workbook.add_format({'bold': True})
 
-    def process_table(table_id, worksheet):
-        col_keys = sorted(obj[table_id].keys())
-        row_keys = sorted(obj[table_id][col_keys[0]].keys())
-        worksheet.title = obj['tables'][table_id]['title']
-        worksheet.merge_range(0, 1, 0, len(col_keys), obj['tables'][table_id]['cols'][2], bold)
-        worksheet.write(1, 0, obj['tables'][table_id]['rows'][2], bold)
-        for col, col_key in enumerate(col_keys, start=1):
-            worksheet.write(1, col, col_key)
-        for row, row_key in enumerate(row_keys, start=2):
-            worksheet.write(row, 0, row_key)
-            for col, col_key in enumerate(col_keys, start=1):
-                worksheet.write(row, col, obj[table_id][col_key][row_key])
+    def render_table(table):
+        worksheet = workbook.add_worksheet(table.title)
+        worksheet.title = table.title
+        # Write column and row titles
+        worksheet.merge_range(
+            0, table.row_dims, 0, table.row_dims + len(table.cols) - 1,
+            table.col_titles[0], bold)
+        for col_ix, row_title in enumerate(table.row_titles):
+            worksheet.write(
+                table.col_dims, col_ix, row_title, bold)
+        # Write column keys
+        for col_ix, col_key in enumerate(table.cols_iter, start=table.row_dims):
+            span = table.col_spans[col_key]
+            for col_dim in range(table.col_dims):
+                if span[col_dim] > 1:
+                    worksheet.merge_range(
+                        col_dim + 1, col_ix,
+                        col_dim + 1, col_ix + span[col_dim] - 1,
+                        col_key[col_dim])
+                elif span[col_dim] == 1:
+                    worksheet.write(col_dim + 1, col_ix, col_key[col_dim])
+        # Write row keys
+        for row_ix, row_key in enumerate(table.rows_iter, start=table.col_dims + 1):
+            span = table.row_spans[row_key]
+            for row_dim in range(table.row_dims):
+                if span[row_dim] > 1:
+                    worksheet.merge_range(
+                        row_ix, row_dim,
+                        row_ix + span[row_dim] - 1, row_dim,
+                        row_key[row_dim])
+                elif span[row_dim] == 1:
+                    worksheet.write(row_ix, row_dim, row_key[row_dim])
+        # Write data
+        for row_ix, row_key in enumerate(table.rows, start=table.col_dims + 1):
+            for col_ix, col_key in enumerate(table.cols, start=table.row_dims):
+                worksheet.write(row_ix, col_ix, table.data[(row_key, col_key)])
 
-    for table_id in obj['tables']:
-        process_table(table_id, workbook.add_worksheet(obj['tables'][table_id]['title']))
+    for table in results:
+        render_table(table)
     workbook.close()
     return stream.getvalue()
-
-
-@register('application/xml', 'XML file', {
-        'Content-Disposition': 'attachment; filename=umansysprop.xml',
-        })
-def render_xml(obj, **kwargs):
-    tag = TagFactory(xml=True)
-
-    def process_table(table_id):
-        col_keys = sorted(obj[table_id].keys())
-        row_keys = sorted(obj[table_id][col_keys[0]].keys())
-        return tag.table(
-            tag.columns(
-                (tag.column(id=col_key) for col_key in col_keys),
-                title=obj['tables'][table_id]['cols'][2]
-                ),
-            tag.rows((
-                tag.row(
-                    (tag.data(value=obj[table_id][col_key][row_key]) for col_key in col_keys),
-                    id=row_key
-                ) for row_key in row_keys),
-                title=obj['tables'][table_id]['rows'][2]
-                ),
-            id=table_id,
-            title=obj['tables'][table_id]['title']
-            )
-
-    return tag.tables(process_table(table_id) for table_id in obj['tables'])
 
 
 @register('text/html', 'HTML (view in web browser)')
 def render_html(obj, **kwargs):
     tag = TagFactory(xml=False)
 
-    def render_table(table_id):
-        col_keys = sorted(obj[table_id].keys())
-        row_keys = sorted(obj[table_id][col_keys[0]].keys())
+    def render_table(table):
         return tag.table(
             tag.caption(obj['tables'][table_id]['title']),
             tag.thead(
                 tag.tr(
-                    tag.th(''),
-                    tag.th(obj['tables'][table_id]['cols'][2], colspan=len(col_keys)),
+                    (tag.th('') for i in range(table.row_dims)),
+                    tag.th(table.col_titles[0], colspan=len(table.cols)),
                     ),
-                tag.tr(
-                    tag.th(obj['tables'][table_id]['rows'][2]),
-                    *(tag.th(key) for key in col_keys)
+                (
+                    tag.tr(
+                        (tag.th('') for i in range(table.row_dims))
+                        if col_dim < table.col_dims - 1 else
+                        (tag.th(row_title) for row_title in table.row_titles),
+                        (
+                            tag.th(key[col_dim], colspan=span if span > 1 else None)
+                            for key in table.cols_iter
+                            for span in (table.col_spans[key][col_dim],)
+                            if span > 0
+                            )
+                        )
+                    for col_dim in range(table.col_dims)
                     )
                 ),
-            tag.tbody((
+            tag.tbody(
                 tag.tr(
-                    tag.th(row_key),
-                    *(tag.td(obj[table_id][col_key][row_key]) for col_key in col_keys)
+                    (
+                        tag.th(row_key[row_dim], rowspan=span if span > 1 else None)
+                        for row_dim in range(table.row_dims)
+                        for span in (table.row_spans[row_key][row_dim],)
+                        if span > 0
+                        ),
+                    (
+                        tag.td(table.data[(data_row, data_col)])
+                        for (data_col, col_key) in zip(table.cols, table.cols_iter)
+                        )
                     )
-                for row_key in row_keys),
+                for (data_row, row_key) in zip(table.rows, table.rows_iter)
                 ),
+            id=table.name,
             )
 
     return tag.div(render_table(table_id) for table_id in obj['tables'])
